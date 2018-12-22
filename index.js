c904cfbde1fcc0fd2a6cfd09cae43420036b6837
sanshi <zhz961101@gmail.com>
2018-12-22 14:11:31 +0800

    tpl:callee

--- index.js
+++ index.js
@@ -2,35 +2,20 @@ const $ = q => document.querySelector.call(document, q);
 (function(window){
     const toArr = o => Array.prototype.slice.call(o)
     const default_html = `<div class="row row-gap"><div class="col"><button type="button" name="button" class=" btn btn-block btn-blue">.primary</button></div><div class="col"><button type="button" name="button" class=" btn btn-block btn-blue ripple">.ripple</button></div><div class="col"><button type="button" name="button" class=" btn btn-block btn-blue ripple-inner">.inner</button></div><div class="col"><button type="button" name="button" class=" btn btn-block btn-blue ripple-outer">.outer</button></div></div>`
+
+    Poi.prototype.mod("node", $("#nodetpl").innerHTML)
     
-    function seletorify($ele) {
-        let selector = $ele.localName
-        if ($ele.id) selector += "#" + $ele.id
-        if ($ele.className) selector += "." + $ele.className.split(" ").join(".")
-        return selector
-    }
     window.app = new Poi({
         el: "#app",
         tpl: "#treeTpl",
         data: {
             innerHtml: "",
-            TreeLi() {
-                let $root = document.createElement("div");
-                $root.innerHTML = this.innerHtml
-    
-                function inner(childs, outer) {
-                    let arr = toArr(childs)
-                    let ret = ""
-                    for (const c of arr) {
-                        ret +=
-                            `<li>
-                                    ${seletorify(c)}
-                                    ${c.children.length != 0?inner(c.children,"ul"):""}
-                                    </li>`
-                    }
-                    return outer ? `<${outer}>${ret}</${outer}>` : ret
-                }
-                return inner($root.children)
+            selfDom: null,
+            seletorify($ele) {
+                let selector = $ele.localName
+                if ($ele.id) selector += "#" + $ele.id
+                if ($ele.className) selector += "." + $ele.className.split(" ").join(".")
+                return selector
             }
         },
         mounted: {
@@ -42,6 +27,10 @@ const $ = q => document.querySelector.call(document, q);
                     console.log("use default template")
                     this.innerHtml = default_html
                 }
+            },
+            renderAfter(){
+                this.selfDom = null;
+                this.selfDom = $("#preBox");
             }
         }
     })
